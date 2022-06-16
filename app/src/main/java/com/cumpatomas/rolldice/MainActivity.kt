ee1e1b5930fc34a82e1a5cc8b73bdbb1e85097a4
cumpatomas <102058754+cumpatomas@users.noreply.github.com>
2022-06-16 18:16:23 +0200

    building app first stage

--- app/src/main/java/com/cumpatomas/rolldice/MainActivity.kt
+++ app/src/main/java/com/cumpatomas/rolldice/MainActivity.kt
@@ -2,10 +2,24 @@ package com.cumpatomas.rolldice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.content.res.AppCompatResources
+import com.cumpatomas.rolldice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setDefaultImage()
+
+    }
+
+    private fun setDefaultImage() {
+        val drawable = AppCompatResources.getDrawable(this, R.drawable.dice_1)
+        binding.ivDice.setImageDrawable(drawable)
     }
 }
