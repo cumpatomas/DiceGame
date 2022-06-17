6266712a827135550131add7440705bf4528485d
Manuel Crovetto <87724520+ManuelCrovetto@users.noreply.github.com>
2022-06-17 19:33:22 +0200

    Expanding MainActivity logic

--- app/src/main/java/com/cumpatomas/rolldice/MainActivity.kt
+++ app/src/main/java/com/cumpatomas/rolldice/MainActivity.kt
@@ -9,11 +9,13 @@ import androidx.appcompat.content.res.AppCompatResources
 import androidx.constraintlayout.motion.widget.MotionLayout
 import com.cumpatomas.rolldice.databinding.ActivityMainBinding
 import kotlin.random.Random
+import kotlin.random.nextInt
 
 private const val TAG = "MainActivity.kt"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var playerTurn = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,72 +24,30 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         setDefaultImage()
         initListeners()
+        assignFirstPlayerTurn()
+    }
+
+    private fun assignFirstPlayerTurn() {
+        val r = (1 until 3).shuffled().last()
+        Log.d(TAG, r.toString())
+        when(r) {
+            1 -> {
+                playerTurn = 1
+                binding.playerOneContainer.background = AppCompatResources.getDrawable(this, R.drawable.active_turn_background)
+            }
+            2 -> {
+                playerTurn = 2
+                binding.playerTwoContainer.background = AppCompatResources.getDrawable(this, R.drawable.active_turn_background)
+            }
+        }
     }
 
     private fun initListeners() {
-        binding.btnRollDice.setOnClickListener { rollDice() }
+        binding.ivDice.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
-        binding.motionLayout.transitionToStart()
-        binding.motionLayout.transitionToEnd()
-        binding.motionLayout.setTransitionListener(object : MotionLayout.TransitionListener{
-            override fun onTransitionStarted(
-                motionLayout: MotionLayout?,
-                startId: Int,
-                endId: Int
-            ) {
 
-            }
-
-            override fun onTransitionChange(
-                motionLayout: MotionLayout?,
-                startId: Int,
-                endId: Int,
-                progress: Float
-            ) {
-                val resourceId = when(Random.nextInt(1,7)) {
-                    1 -> R.drawable.dice_1
-                    2 -> R.drawable.dice_2
-                    3 -> R.drawable.dice_3
-                    4 -> R.drawable.dice_4
-                    5 -> R.drawable.dice_5
-                    else -> { R.drawable.dice_6 }
-                }
-                binding.ivDice.setImageDrawable(AppCompatResources.getDrawable(this@MainActivity, resourceId))
-            }
-
-            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
-                val resourceId = when(Random.nextInt(1,7)) {
-                    1 -> R.drawable.dice_1
-                    2 -> R.drawable.dice_2
-                    3 -> R.drawable.dice_3
-                    4 -> R.drawable.dice_4
-                    5 -> R.drawable.dice_5
-                    else -> { R.drawable.dice_6 }
-                }
-                binding.ivDice.setImageDrawable(AppCompatResources.getDrawable(this@MainActivity, resourceId))
-            }
-
-            override fun onTransitionTrigger(
-                motionLayout: MotionLayout?,
-                triggerId: Int,
-                positive: Boolean,
-                progress: Float
-            ) {
-                TODO("Not yet implemented")
-            }
-
-        })
-        val resourceId = when(Random.nextInt(1,7)) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> { R.drawable.dice_6 }
-        }
-        binding.ivDice.setImageDrawable(AppCompatResources.getDrawable(this, resourceId))
     }
 
     private fun setDefaultImage() {
